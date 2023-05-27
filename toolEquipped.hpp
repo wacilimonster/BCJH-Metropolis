b6c58de06c4f9a624d18ae4a5d1110d33027d9b9
wacilimonster <30611658+wacilimonster@users.noreply.github.com>
2023-05-27 22:41:56 +0800

    Update toolEquipped.hpp

--- toolEquipped.hpp
+++ toolEquipped.hpp
@@ -30,19 +30,13 @@ void toolEquipped(Chef *chef) {
 
     chef->tool = NO_TOOL;
 
-    ///////////// 自定义厨师装备的厨具：
+ 自定义厨师装备的厨具：
 
     auto skill = &chef->skill; // 这句不用管，放着就好。
 
-    if (chef->name == "达浪") {              // 金锄头
-        skill->ability.add(30);              // 全技法+30
-        skill->materialBuff.vegetable += 15; // 蔬菜售价+15%
+    if (chef->name == "姒露") {              // 金开六味瓶        // 全技法+30
+        skill->abilityBuff.bake += 20; // 蔬菜售价+15%
         chef->tool = NO_TOOL;                // 不允许装备其他厨具
-    }
-    if (chef->name == "宋书汐汐") {    // 幸福礼花炮
-        skill->ability.stirfry += 150; // 炒技法+150
-        skill->abilityBuff.bake += 25; // 烤售价+25%
-        chef->tool = NO_TOOL;          // 不允许装备其他厨具
     }
 }
 #endif
